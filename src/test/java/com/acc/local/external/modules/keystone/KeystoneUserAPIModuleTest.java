4bfa76ac1e71a5d6bafa728a26f20d6707048120
waffle <nate2402@ajou.ac.kr>
2026-01-05 01:20:52 +0900

    revert: refactor 간 명칭변경 오류 class 원복

--- src/test/java/com/acc/local/external/modules/keystone/KeystoneUserAPIModuleTest.java
+++ src/test/java/com/acc/local/external/modules/keystone/KeystoneUserAPIModuleTest.java
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
-class KeystoneUserKeystoneAPIModuleTest {
+class KeystoneUserAPIModuleTest {
 
 	@Mock
 	private OpenstackAPICallModule openstackAPICallModule;
--- src/test/java/com/acc/local/service/modules/auth/UserModuleTest.java
+++ src/test/java/com/acc/local/service/modules/auth/UserModuleTest.java
@@ -39,7 +39,7 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-class UserKeystoneModuleTest {
+class UserModuleTest {
 
     @Mock
     private KeystoneAPIExternalPort keystoneAPIExternalPort;
