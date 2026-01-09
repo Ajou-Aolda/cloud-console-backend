0285e4a503632dccb4a762f365ef6a561020f2fc
천 진강 <jjjjjk12@ajou.ac.kr>
2026-01-09 16:45:00 +0900

    feat: 서브넷 목록 조회, 삭제 external 계층 로직 작성

--- src/main/java/com/acc/local/external/ports/NeutronSubnetExternalPort.java
+++ src/main/java/com/acc/local/external/ports/NeutronSubnetExternalPort.java
@@ -1,6 +1,8 @@
 package com.acc.local.external.ports;
 
+import com.acc.global.common.PageResponse;
 import com.acc.local.dto.network.CreateNetworkRequest;
+import com.acc.local.dto.network.ViewSubnetsResponse;
 
 import java.util.List;
 import java.util.Map;
@@ -8,4 +10,6 @@ import java.util.Map;
 public interface NeutronSubnetExternalPort {
 
     List<Map<String, String>> callCreateSubnet(String keystoneToken, List<CreateNetworkRequest.Subnet> subnets, String networkId);
+    void callDeleteSubnet(String keystoneToken, String subnetId);
+    PageResponse<ViewSubnetsResponse> callListSubnets(String keystoneToken, String networkId, String marker, String direction, int limit);
 }
